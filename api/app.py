b93fc15baea75d4526da9b1cc4f3041f7bc57c41
bluesibra <41315308+bluesibra@users.noreply.github.com>
2021-02-21 13:11:54 -0500

    modified app.py and index.html

--- api/app.py
+++ api/app.py
@@ -3,16 +3,33 @@ app=flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
 def home():
     return "<h1>BLueprint Project</h1><p>hello world .</p>"
 
-@app.route('/', methods=['POST'])
-def image_captioning_function():
-    return 'image captioning here'
+@app.route('/img_txt', methods=['POST', 'GET'])
+def image_text_summary():
+    request_data = request.get_json()
 
-@app.route('/', methods=['POST'])
-def text_summarization():
-    return 'text summaries here'
+    language = request_data['language']
+    framework = request_data['framework']
 
+    # two keys are needed because of the nested object
+    python_version = request_data['version_info']['python']
+
+    # an index is needed because of the array
+    example = request_data['examples'][0]
+
+    boolean_test = request_data['boolean_test']
+
+    return '''
+           The language value is: {}
+           The framework value is: {}
+           The Python version is: {}
+           The item at index 0 in the example list is: {}
+           The boolean value is: {}'''.format(language, framework, python_version, example, boolean_test)
+ 
+
+    return 'RESULTS here'
+   
 app.run()
